62b4c9cd89ebb698cdd3236cd536ef11b65882e8
Roger Mao <roger.mao@gmail.com>
2016-04-08 01:04:07 -0500

    finished and tested signal tests. Revamped registration tests

--- registration_test.cpp
+++ registration_test.cpp
@@ -8,12 +8,14 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <time.h>
 
 #define SIGTEST 44
 #define SIGSEVERE 45
 #define SIGMIN 46
 
-volatile int registered = 0;
+volatile int received = 0;
+
 static int getDaemonPID(){
 	FILE *in = popen("pgrep daemon","r");
 	int pid = 0;
@@ -24,37 +26,36 @@ static int getDaemonPID(){
 	return pid;
 }
 
-ATF_TEST_CASE(deregistration);
-ATF_TEST_CASE_HEAD(deregistration)
-{
-	set_md_var("descr", "This tests deregistration of applications");
+void receiveSignal(int n, siginfo_t *info, void *unused){
+	received = 1;	
 }
-ATF_TEST_CASE_BODY(deregistration)
-{
-	int pid = getDaemonPID();
+
+void setupHandler(int signal){
+	struct sigaction sig;
+	sig.sa_sigaction = receiveSignal;
+	sig.sa_flags = SA_SIGINFO | SA_RESETHAND;
+	sigaction(signal, &sig, NULL); 
+}
+
+int registerApp(int pid, int signal){
 
 	siginfo_t *info;
-	info->si_pid = 999;
+	info->si_pid = pid;
 	void* unused;	
 
-	monitor_application(SIGSEVERE, info, unused);
+	monitor_application(signal, info, unused);
 
-	int result = isRegistered(999);
+	int result = isRegistered(pid);
 
-	if(result == 0){
-		ATF_FAIL("Registration Failed");	
-	}	
+	return result;
+}
 
-	monitor_application(SIGSEVERE, info, unused);
+void sleepFor(int secs){
+	struct timespec sleepInterval; 
+	sleepInterval.tv_sec = secs;
+	sleepInterval.tv_nsec = 0; 
 
-	result = isRegistered(999);
-
-	if(!result){
-		ATF_PASS();
-	}
-	else{
-		ATF_FAIL("Not Deregistered");
-	}	
+	nanosleep(&sleepInterval, 0); 
 }
 
 ATF_TEST_CASE(registration);
@@ -64,16 +65,9 @@ ATF_TEST_CASE_HEAD(registration)
 }
 ATF_TEST_CASE_BODY(registration)
 {
-
-	int pid = getDaemonPID();
-
-	siginfo_t *info;
-	info->si_pid = 999;
-	void* unused;	
-
-	monitor_application(SIGSEVERE, info, unused);
-
-	int result = isRegistered(999);
+	int app_pid = getpid();
+	
+	int result = registerApp(app_pid, SIGSEVERE);
 
 	if(result == 0){
 		ATF_FAIL("Registration Failed");	
@@ -82,134 +76,174 @@ ATF_TEST_CASE_BODY(registration)
 	}
 }
 
+ATF_TEST_CASE(deregistration);
+ATF_TEST_CASE_HEAD(deregistration)
+{
+	set_md_var("descr", "This tests deregistration of applications");
+}
+ATF_TEST_CASE_BODY(deregistration)
+{
+	int app_pid = getpid();
+	
+	int result = registerApp(app_pid, SIGSEVERE);
+
+	if(result == 0){
+		ATF_FAIL("Registration Failed");	
+	}	
+	
+	//Sending registration signal again should deregister	
+	result = registerApp(app_pid, SIGSEVERE);
+	
+	if(!result){
+		ATF_PASS();
+	}
+	else{
+		ATF_FAIL("Not Deregistered");
+	}	
+}
+
 ATF_TEST_CASE(sigmin);
 ATF_TEST_CASE_HEAD(sigmin)
 {
-	set_md_var("descr", "This test if sigmin signal triggered 
-properly");
+	set_md_var("descr", "This test if sigmin signal triggered properly");
 }
 ATF_TEST_CASE_BODY(sigmin)
 {
+	received = 0;
 
-	kill(pid, SIGMIN);
+	int app_pid = getpid();
+
+	setupHandler(SIGTEST);
 	
-	struct sigaction sig;
-	sig.sa_sigaction = receiveData;
-	sig.sa_flags = SA_SIGINFO;
-	sigaction(SIGTEST, &sig, NULL); 
-	
+	int result = registerApp(app_pid, SIGMIN);
+
+	if(result == 0){
+		ATF_FAIL("Application Failed to Register");
+	}
+
 	srand(time(NULL));
 	int *mem;
 	int memsize=1024; 
-	mem=malloc(memsize);
+	mem= (int*)malloc(memsize);
 	
-	while(!pausevar){
-		mem=realloc(mem,memsize=memsize+1024*1024*10);
+	while(queryDev().min){
+		mem= (int*)realloc(mem,memsize=memsize+1024*1024*10);
 		sleep(1);
 	}
-	
-	mem = realloc(mem,memsize = memsize/8);
-	sem_wait(&sem);
-	pausevar=0;
-	sem_post(&sem);
-	
-	ATF_PASS();
-	
-	ATF_FAIL("THIS SHOULD NEVER FAIL");
 
+	check_applications(false, true, false);
+
+	sleepFor(5);
+
+	if(received){
+		ATF_PASS();
+	}else{
+		ATF_FAIL("SIGMIN not received");
+	}
+	
 }
+
+
 ATF_TEST_CASE(sigpagesneeded);
 ATF_TEST_CASE_HEAD(sigpagesneeded)
 {
-	set_md_var("descr", "This test if sigpagesneeded signal 
-triggered properly");
+	set_md_var("descr", "This test if sigpagesneeded signal triggered properly");
 }
 ATF_TEST_CASE_BODY(sigpagesneeded)
 {
+	received = 0;
 
-	kill(pid, SIGMIN);
-	
-	struct sigaction sig;
-	sig.sa_sigaction = receiveData;
-	sig.sa_flags = SA_SIGINFO;
-	sigaction(SIGTEST, &sig, NULL); 
-	
+	int app_pid = getpid();
+
+	setupHandler(SIGTEST);
+
+	int result = registerApp(app_pid, SIGPAGESNEEDED);
+
+	if(result == 0){
+		ATF_FAIL("Application Failed to Register");
+	}
+
 	srand(time(NULL));
 	int *mem;
 	int memsize=1024; 
-	mem=malloc(memsize);
+	mem= (int*)malloc(memsize);
 	
-	while(!pausevar){
-		mem=realloc(mem,memsize=memsize+1024*1024*10);
+	while(queryDev().needed){
+		mem=(int*)realloc(mem,memsize=memsize+1024*1024*10);
 		sleep(1);
 	}
-	
-	mem = realloc(mem,memsize = memsize/8);
-	sem_wait(&sem);
-	pausevar=0;
-	sem_post(&sem);
-	
-	ATF_PASS();
-	
-	ATF_FAIL("THIS SHOULD NEVER FAIL");
+
+	check_applications(false, false, true);
+
+	sleepFor(5);
+
+	if(received){
+		ATF_PASS();
+	}else{
+		ATF_FAIL("SIGPAGESNEEDED not received");
+	}
+
 
 }
+
+//DISABLED: Imossible to override signal handler
 ATF_TEST_CASE(sigstop);
 ATF_TEST_CASE_HEAD(sigstop)
 {
-	set_md_var("descr", "This test if sigmin signal triggered 
-properly");
+	set_md_var("descr", "This test suspend_applications");
 }
 ATF_TEST_CASE_BODY(sigstop)
 {
+	received = 0;
 
-	kill(pid,SIGMIN);
-	
-	struct sigaction sig;
-	sig.sa_sigaction = receiveData;
-	sig.sa_flags = SA_SIGINFO;
-	sigaction(SIGSTOP, &sig, NULL); 
-	
-	while(i < timeoutCount){
-		nanosleep(&sleepInterval, 0); 
-		if(pausevar==1){
-			ATF_PASS();
-		}
+	int app_pid = getpid();
+
+	setupHandler(SIGSTOP);
+
+	int result = registerApp(app_pid, SIGSEVERE);
+
+	if(result == 0){
+		ATF_FAIL("Application Failed to Register");
 	}
-	
+
+	suspend_applications();	
+
+	sleepFor(5);
+
+	if(received){
+		ATF_PASS();
+	}
 	ATF_FAIL("THIS SHOULD NEVER FAIL");
 
 }
 ATF_TEST_CASE(sigcont);
 ATF_TEST_CASE_HEAD(sigcont)
 {
-	set_md_var("descr", "This test if sigmin signal triggered 
-properly");
+	set_md_var("descr", "This test resume_applications");
 }
 ATF_TEST_CASE_BODY(sigcont)
 {
+	received = 0;
 
-	kill(pid,SIGMIN);
-	
-	struct sigaction sig;
-	sig.sa_sigaction = receiveData;
-	sig.sa_flags = SA_SIGINFO;
-	sigaction(SIGCONT, &sig, NULL); 
-	
-	int i = 0;
-	struct timespec sleepInterval; 
-	sleepInterval.tv_sec = 1;
-	sleepInterval.tv_nsec = 0; 
-	int timeoutCount = 10; 
-	
-	while(i < timeoutCount){
-		nanosleep(&sleepInterval, 0); 
-		if(pausevar==1){
-			ATF_PASS();
-		}
+	int app_pid = getpid();
+
+	setupHandler(SIGCONT);
+
+	int result = registerApp(app_pid, SIGSEVERE);
+
+	if(result == 0){
+		ATF_FAIL("Application Failed to Register");
 	}
-	
+
+	resume_applications();	
+
+	sleepFor(5);
+
+	if(received){
+		ATF_PASS();
+	}
 	ATF_FAIL("THIS SHOULD NEVER FAIL");
+
 
 }
 ATF_INIT_TEST_CASES(tcs)
@@ -218,6 +252,6 @@ ATF_INIT_TEST_CASES(tcs)
 	ATF_ADD_TEST_CASE(tcs, registration);
 	ATF_ADD_TEST_CASE(tcs, sigmin);
 	ATF_ADD_TEST_CASE(tcs, sigpagesneeded);
-	ATF_ADD_TEST_CASE(tcs, sigstop);
+	//ATF_ADD_TEST_CASE(tcs, sigstop);
 	ATF_ADD_TEST_CASE(tcs, sigcont);
 }
